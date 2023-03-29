da29bce4b95f71b015e0abdb2b94079d247e9b00
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-03-29 13:21:13 -0600

    Inicio de proyecto - JournalApp

---
+++ src/JournalApp.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+function JournalApp() {
+//  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+      <h1> Hola Mundo</h1>
+      </div>
+       
+    </div>
+  )
+}
+
+export default JournalApp
