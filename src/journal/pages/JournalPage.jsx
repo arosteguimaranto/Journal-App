563eef96f70d2a7cea358482aa6f245099b244f6
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-04-06 16:07:32 -0600

    NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,16 @@
 
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 
 
 
 export const JournalPage = () => {
-  return (  
+  return (
     <JournalLayout>
-   <NothingSelectedView/>
-    
+      {/* <NothingSelectedView/> */}
+
+      <NoteView />
     </JournalLayout>
 
   )
