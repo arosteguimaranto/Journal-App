d137e9e4a47f93eb640b1c7e3c6f0259cc012fd7
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-06-04 12:46:12 -0600

    Actualizar el displayName y autenticar el usuario

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth';
 import { FirebaseAuth } from './config';
 
 const googleProvider = new GoogleAuthProvider();
@@ -34,8 +34,18 @@ export const registerUserWithEmailPassword = async ({ email, password, displayNa
 
         const resp = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
         const { uid, photoURL } = resp.user;
-        console.log(resp);
+         //TODO: actualizar el displayName en Firebase
 
+       await updateProfile(FirebaseAuth.currentUser,{displayName});
+
+
+
+       
+
+        return{
+            ok: true,
+            uid, photoURL, email, displayName
+        }
 
     } catch (error) {
         console.log(error)
