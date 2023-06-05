7392c0534cfc79a0b4b3c9db9510f8a8c0ef8ae7
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-06-05 12:04:24 -0600

    Mantener el estado de la autenticación al recargar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,27 +1,58 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalPage } from '../journal/pages/JournalPage';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { CheckingAuth } from '../ui';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FirebaseAuth } from '../firebase/config';
+import { logout,login } from '../store/auth';
+import { JournalRoutes } from '../journal/routes/JournalRoutes';
 
 export const AppRouter = () => {
 
-   const {status} = useSelector(state => state.auth);
+  const { status } = useSelector(state => state.auth);
 
-   if( status === 'checking'){
-    return <CheckingAuth/>
-   }
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+     if(!user) return dispatch(logout());
+      const {uid, email, displayName, photoURL} = user;
+     dispatch(login({uid, email, displayName, photoURL }));
+
+    })
+
+
+  }, [])
+
+
+
+  if (status === 'checking') {
+    return <CheckingAuth />
+  }
+
 
   return (
     <Routes>
 
-    {/* Login y Registro */}
-    <Route path="/auth/*" element={<AuthRoutes/>} />
+    {
+      (status === 'authenticated')
+      ? <Route path="/*" element={<JournalRoutes />} />
+      :  <Route path="/auth/*" element={<AuthRoutes />} />
+    }
+ 
+    <Route path='/*' element={<Navigate to='/auth/login'/>} />
 
-     {/* JournalApp  */}
-     <Route path="/*" element={<JournalPage/>}/>
+      {/* Login y Registro */}
+      {/*<Route path="/auth/*" element={<AuthRoutes />} />*/}
 
-     
+      {/* JournalApp  */}
+      {/*<Route path="/*" element={<JournalPage />} />*/}
+
+
     </Routes>
   )
 }
