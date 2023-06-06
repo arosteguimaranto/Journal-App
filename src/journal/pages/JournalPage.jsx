792206c3cdfbcf94e8486a6dfd77cbce3b09208a
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-06-06 13:34:07 -0600

    Crear una nueva nota

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -4,10 +4,19 @@ import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 import { AddOutlined } from "@mui/icons-material"
+import { useDispatch } from "react-redux"
+import { startNewNote } from "../../store/journal/thunks"
 
 
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () =>{
+    dispatch(startNewNote());
+  }
+
   return (
     <JournalLayout>
       {<NothingSelectedView/>}
@@ -16,6 +25,7 @@ export const JournalPage = () => {
 
 
     <IconButton
+    onClick={onClickNewNote}
     size='large'
     sx={{
       color: 'white',
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,10 @@
-export const startNewNote = () => {
-    return async (dispatch) => {
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FireBaseDB } from "../../firebase/config";
 
+export const startNewNote = () => {
+    return async (dispatch, getState) => {
+
+         const {uid}= getState().auth;
         // uid
 
         const newNote = {
@@ -8,6 +12,11 @@ export const startNewNote = () => {
             body: '',
             date: new Date().getTime(),
         }
+
+        const newDoc = doc(collection(FireBaseDB,`${uid}/journal/notes`));
+        const setDocResp = await setDoc(newDoc, newNote);
+        
+        console.log({newDoc, setDocResp});
 
         //dispatch
         //dispatch( newNote)
