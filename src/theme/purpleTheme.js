5a36c6ce9bb7e3b0525e57e0abef95b69101df7a
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-06-07 19:11:42 -0600

    Nuevo color

--- src/theme/purpleTheme.js
+++ src/theme/purpleTheme.js
@@ -4,7 +4,7 @@ import{red} from '@mui/material/colors';
 export const purpleTheme = createTheme({
     palette: {
         primary: {
-            main: '#9A2EFE'
+            main: '#262254'
         },
         secondary: {
             main: '#BE81F7'
