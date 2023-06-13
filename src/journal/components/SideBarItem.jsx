2955d361aa29224f5bf80ed5cf78db55fae92538
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-06-13 13:21:55 -0600

    Activar una nota para su edición

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -14,7 +14,7 @@ export const SideBarItem = ({ title = '', body, id, date, imageUrls = [] }) => {
     }
 
 
-    const newTitle = useMemo(() => {
+    const newTitle = useMemo(() => {    
         return title.length > 17
             ? title.substring(0, 17) + '...'
             : title;
--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,15 +1,29 @@
 import { SaveOutlined } from '@mui/icons-material'
 import { Button, Grid, TextField, Typography } from '@mui/material'
-
+import { useSelector } from 'react-redux'
 import React from 'react'
 import { ImageGallery } from '../components/ImageGallery'
+import { useForm } from '../../hooks/useForm'
+import { useMemo } from 'react'
 
 export const NoteView = () => {
+
+    const { active: note } = useSelector(state => state.journal);
+    const { body, title, date, onInputChange, formState } = useForm(note);
+
+    const dateString = useMemo(() => {
+
+        const newDate = new Date(date);
+
+        return newDate.toUTCString();
+    }, [date])
+
+
     return (
         <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}>
-            
-        <Grid item>
-                <Typography fontSize={39} fontWeight='light'> 28 de Agosto, 2023 </Typography>
+
+            <Grid item>
+                <Typography fontSize={39} fontWeight='light'> {dateString} </Typography>
             </Grid>
             <Grid item>
                 <Button color='primary' sx={{ padding: 2 }}>
@@ -27,19 +41,25 @@ export const NoteView = () => {
                     label="Titulo"
                     sx={{ border: 'none', mb: 1 }}
                     className='animate__animated animate__slideInLeft'
+                    name='title'
+                    value={title}
+                    onChange={onInputChange}
                 />
                 <TextField
                     type="text"
                     variant="filled"
                     fullWidth
                     multiline
-                    placeholder="¿Que sucedió hoy?"
+                    placeholder="¿Que sucedió el dia de hoy?"
                     minRows={5}
+                    name='body'
+                    value={body}
+                    onChange={onInputChange}
                 />
             </Grid>
-        {/*Image gallery */}
+            {/*Image gallery */}
 
-        <ImageGallery/>
+            <ImageGallery />
 
         </Grid>
     )
